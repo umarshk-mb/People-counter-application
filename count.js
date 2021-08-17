d43662ca1ea01665a231c400f67813074065d6f8
Mohammed Umer MB <umarbvt4430@gmail.com>
2021-08-17 21:35:28 +0530

    first commit

---
+++ count.js
@@ -0,0 +1,16 @@
+let count=0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+
+function increment() {
+    count += 1
+    countEl.innerText = count
+}
+
+function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
